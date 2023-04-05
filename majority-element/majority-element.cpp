e3c0b5a93fcc981bdc1f546643bf7a5b665584c6
Tanvi Saxena <83595033+tanvisaxena1901@users.noreply.github.com>
2023-04-05 17:48:08 +0530

    Time: 12 ms, Memory: 19.6 MB - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        //solving by boyer moore majority voting algorithm
+        int n=nums.size();
+        int cnt=0,candi=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(cnt==0)
+            {
+                candi=nums[i];
+                cnt=1;
+            }
+            else
+            {
+                if(nums[i]==candi)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt--;
+                }
+            }
+        }
+    
+      
+        return candi;
+        
+    }
+};
