b9347ff73d7f5c718653e82ff657915333198514
Tanvi Saxena <83595033+tanvisaxena1901@users.noreply.github.com>
2023-07-16 15:19:33 +0530

    Time: 5 ms (22.79%), Space: 6.4 MB (48.80%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>v(m,vector<int>(n,1));
+        if(m==1 && n==1)return 1;
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                v[i][j] = v[i][j-1]+v[i-1][j];
+            }
+        }
+        return v[m-1][n-1];
+    }
+};
